f5d733c8f0a50b4a96bdcd796125abdf8019f256
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-08-03 15:15:51 +0700

    responsvie doc truyen1

--- src/components/HeaderFile.js
+++ src/components/HeaderFile.js
@@ -105,7 +105,10 @@ export default function HeaderFile() {
 
           </div>
           <div className="w-2/12 text-right flex flex-row justify-between">
-            <button className='navbar-nav__collapse fs-14 ' onClick={toggleUserMenu}><i className="fa-solid fa-bars"></i></button>
+            {user ?
+              <button className='navbar-nav__collapse fs-14 ' onClick={toggleUserMenu}><i className="fa-solid fa-bars"></i></button>
+              : null
+            }
             {user ?
               <button className='navbar-nav__collapse fs-14 ' onClick={onClickLogout}>
                 <i className="fa fa-sign-in-alt"></i>
