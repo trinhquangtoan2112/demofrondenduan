0eccee0d8f01554682309e9fecb88d5e7b403d14
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2024-08-04 16:48:18 +0700

    1422424

--- src/layout/Chapter/Chapter.js
+++ src/layout/Chapter/Chapter.js
@@ -239,7 +239,7 @@ function Chapter(props) {
                       {/*header*/}
                       <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                         <h3 className="text-3xl font-semibold">
-                          Modal Title
+                          Chỉnh sửa
                         </h3>
                         <button
                           className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
